6923f9be7d1f538a1b85efdb60f46c0f9bb72753
Kyoku <henji0905@gmail.com>
2025-04-03 22:12:41 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import jsconfigPath from "vite-jsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),jsconfigPath()],
-  base: "/ticketform/",
+  base: "/ticketForm/",
   build: {
     chunkSizeWarningLimit: 1000, // Устанавливаем порог в 1000 кБ
   }
