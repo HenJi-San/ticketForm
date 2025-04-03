db84f24ee371f713d67e772d44a2053d813f3b45
Kyoku <henji0905@gmail.com>
2025-04-03 22:03:09 +0500

    size limit in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import jsconfigPath from "vite-jsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),jsconfigPath()],
-  base: "/ticketform/"
+  base: "/ticketform/",
+  build: {
+    chunkSizeWarningLimit: 1000, // Устанавливаем порог в 1000 кБ
+  }
 })
